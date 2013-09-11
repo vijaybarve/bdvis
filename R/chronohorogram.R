aba27638cd6c53fdaf025bfc5622cfaf3a98d74c
VijayBarve <vjaybarve@rediffmail.com>
2013-09-11 17:23:32 -0500

    V 0.0.6 Added fxn chronohorogram

---
+++ R/chronohorogram.R
@@ -0,0 +1,33 @@
+#'chronohorogram - Draws a chronohorogram of records
+#'@import sqldf
+#'@import plotrix
+#'@param indf input data frame containing biodiversity data set
+#'@param title title of the plot
+#'@param startyear Starting year for the plot
+#'@param endyear End year for the graph
+#'@export
+#'@examples \dontrun{
+#'chronohorogram(inat)
+#'}
+chronohorogram <- function (indf=NA,title=NA,startyear=0,endyear=0){
+  if (!is.na(title)) {
+    title2 <- title
+  } else {
+    title2 <- "Chronohorogram"
+  }
+  if (startyear==0){
+    startyear=1980
+  } 
+  if (endyear==0){
+    endyear=2015
+  }
+  dat1=sqldf("select Date_collected, count(*) as ct from indf group by Date_collected")
+  if(dat1$Date_collected[1]==""){dat1=dat1[2:dim(dat1)[1],]}
+  d=as.numeric(strftime(as.Date(dat1$Date_collected,na.rm=T), format = "%j"))
+  y=as.numeric(strftime(as.Date(dat1$Date_collected,na.rm=T), format = "%Y"))
+  radial.plot(y,(d/366)*360,rp.type="s", start=1.62, labels=month.abb,
+              clockwise=TRUE, point.col=round(log10(dat1[,2])+2), point.symbols=20
+              ,grid.bg="black",radial.lim=c(startyear,endyear),
+              show.radial.grid=T,show.grid.labels=F,show.grid=T,
+              main=title2)
+}
