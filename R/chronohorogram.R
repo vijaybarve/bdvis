432bd72608b799619eaff2409290f7fc1c75d7b4
VijayBarve <vjaybarve@rediffmail.com>
2013-12-21 19:17:20 -0600

    v 0.0.13 fxn chronohorogram has colors param to control colors

--- R/chronohorogram.R
+++ R/chronohorogram.R
@@ -5,11 +5,13 @@
 #'@param title - Title of the plot
 #'@param startyear - Starting year for the plot
 #'@param endyear - End year for the graph
+#'@param colors - Colors to build color ramp 
 #'@export
 #'@examples \dontrun{
 #'chronohorogram(inat)
 #'}
-chronohorogram <- function (indf=NA,title=NA,startyear=0,endyear=0){
+chronohorogram <- function (indf=NA,title=NA,startyear=0,endyear=0,
+                            colors=c("red", "blue")){
   if (!is.na(title)) {
     title2 <- title
   } else {
@@ -29,10 +31,10 @@ chronohorogram <- function (indf=NA,title=NA,startyear=0,endyear=0){
   rind=which(y<=endyear & y>=startyear)
   d=d[rind]
   y=y[rind]
-  
+  crp=colorRampPalette(colors)( round(log10(max(dat1$ct)+2)) +1 ) 
   radial.plot(y,(d/366)*360,rp.type="s", start=1.62, labels=month.abb,
-              clockwise=TRUE, point.col=round(log10(dat1[,2])+2), point.symbols=20
-              ,grid.bg="black",radial.lim=c(startyear,endyear),
+              clockwise=TRUE, point.col=crp[(round(log10(dat1[,2])+2))-1], 
+              point.symbols=20, grid.bg="black",radial.lim=c(startyear,endyear),
               show.radial.grid=T,show.grid.labels=F,show.grid=T,
               main=title2)
 }
