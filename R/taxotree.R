3ff7e29d39c146b15dc3892d9187730f60cef708
VijayBarve <vjaybarve@rediffmail.com>
2014-06-09 12:28:38 +0530

    v 0.1.3 param n, title, legend added for taxotree #16

--- R/taxotree.R
+++ R/taxotree.R
@@ -2,15 +2,29 @@
 #'@import sqldf
 #'@import treemap
 #'@param indf input data frame containing biodiversity data set
+#'@param n max number of rectangles to be plotted in the treemap
+#'@param title title for the tree
+#'@param legend legend title
 #'@references Otegui, J., Arino, A. H., Encinas, M. A., & Pando, F. (2013). Assessing the Primary Data Hosted by the Spanish Node of the Global Biodiversity Information Facility (GBIF). PLoS ONE, 8(1), e55144. doi:10.1371/journal.pone.0055144
 #'@export
 #'@examples \dontrun{
 #'taxotree(inat)
 #'}
-taxotree <- function(indf){
+taxotree <- function(indf,n=30,title=NA,legend=NA){
+  if (!is.na(title)) {
+    title2 <- title
+  } else {
+    title2 <- "Records per family"
+  }
+  if (!is.na(legend)) {
+    legend2 <- legend
+  } else {
+    legend2 <- "Number of Genus"
+  }
+  
   tab1=sqldf("select Family,Genus,count(*) as Recs from indf group by Family,Genus ")
   tab2=sqldf("select Family,count(*) as Gnum, sum(Recs) as Rec1 from tab1 group by Family order by Rec1")
-  tab2=tail(tab2,30)
+  tab2=tail(tab2,n)
   treemap(tab2, index=c("Family"), vSize="Rec1", vColor="Gnum", type="value",
-          title="Records per family", title.legend="Number of Genus")
+          title=title2, title.legend=legend2)
 }
