e0f584672954caa2e19a70f75395f488d46d97cc
VijayBarve <vjaybarve@rediffmail.com>
2016-02-21 23:01:22 -0500

    v 0.1.29 getcellid issue #54

--- R/getcellid.R
+++ R/getcellid.R
@@ -1,6 +1,4 @@
 #'Assigns GBIF style degree cell ids
-#'
-#'
 #'  
 #'Assigns GBIF style degree cell ids and Centi degree (0.1) cell ids for each record which are used in functions like \link[bdvis]{mapgrid}
 #'  
@@ -10,6 +8,8 @@
 #'getcellid(inat)
 #'}
 getcellid <- function (indf){
+  indf$Latitude <- as.numeric(indf$Latitude)
+  indf$Longitude <- as.numeric(indf$Longitude)
   indf$Cell_id <- (((indf$Latitude %/% 1) + 90) * 360) + ((indf$Longitude %/% 1) + 180)
   indf$Centi_cell_id <- ((((indf$Latitude %% 1) * 10) %/% 1 ) * 10) + (((indf$Longitude %% 1) * 10) %/% 1)
   return(indf)
