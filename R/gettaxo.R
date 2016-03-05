a163746382bf94b7caf379325d7eeb5cf9bbd10a
VijayBarve <vjaybarve@rediffmail.com>
2016-03-04 20:53:26 -0500

    v 0.1.35 fxn gettaxo has verbose option now #67

--- R/gettaxo.R
+++ R/gettaxo.R
@@ -7,6 +7,7 @@
 #'@import sqldf
 #'@param indf input data frame containing biodiversity data set
 #'@param genus If true use only genus level data to get taxanomy
+#'@param verbose IF true displays each name string for which the higher taxonomy is sought
 #'@return indf with added / updated columns
 #'\itemize{
 #'  \item{"Kingdom"}{Kingdom of the Scientific name}
@@ -20,13 +21,14 @@
 #'inat=gettaxo(inat)
 #'}
 #'@export
-gettaxo <- function(indf,genus=FALSE){
+gettaxo <- function(indf,genus=FALSE,verbose=FALSE){
+  names(indf)[names(indf)=="Order_"]<-"Order"
   indfu=sqldf("select Scientific_name from indf group by Scientific_name order by Scientific_name")
   if(file.exists("taxo.db")){
     mytaxo=sqldf("select * from taxo", dbname = "taxo.db")
     indfu=sqldf("select indfu.*, mytaxo.* from indfu left outer join mytaxo on  indfu.Scientific_name = mytaxo.Scientific_name")
     indfu=indfu[,-c(2)]
-    print("Local database taxo.db is present")
+    cat("Local database taxo.db is present\n")
   } else {
     indfu$Kingdom[1]=NA
     indfu$Phylum[1]=NA
@@ -34,17 +36,17 @@ gettaxo <- function(indf,genus=FALSE){
     indfu$Order[1]=NA
     indfu$Family[1]=NA
     indfu$Genus[1]=NA
-    print("Local database taxo.db is absent")
+    cat("Local database taxo.db is absent\n")
   }
   indfu1=indfu[which(!is.na(indfu$Kingdom)),]
   indfu=indfu[which(is.na(indfu$Kingdom)),]
-  #indfu1=indfu[which(indfu$Kingdom!=""),]
-  #indfu=indfu[which(indfu$Kingdom==""),]
   if(dim(indfu)[1]>0){
-    print("Processing names not present in local database ...")
+    cat("Processing names not present in local database ...\n")
     sciname=""
     dat1<-NULL
     for(i in 1:dim(indfu)[1]){
+      if(dim(indfu)[1]<=1){pbar=2} else {pbar=dim(indfu)[1]}
+      pb   <- txtProgressBar(1, pbar, style=3)
       if(genus){
         Scientific_name=strsplit(indfu$Scientific_name[i]," ")[[1]][1]
         if(is.na(Scientific_name)){Scientific_name=""}
@@ -52,7 +54,7 @@ gettaxo <- function(indf,genus=FALSE){
         Scientific_name=indfu$Scientific_name[i]
       }
       if (!Scientific_name==sciname){
-        dat <- classification(get_uid(Scientific_name))
+        dat <- classification(get_uid(Scientific_name,verbose = verbose))
         if(!is.na(dat)){
           dat1<-NULL
           dat1 <- ldply(dat, function(x) x[x$rank %in% c("kingdom","phylum","class","order","family","genus","species"), "name"])
@@ -61,7 +63,6 @@ gettaxo <- function(indf,genus=FALSE){
       }
       if(!is.null(dat1)) {
         names(dat1) <- dat2
-        if(!is.null(dat1)) print(dat1)
         if(!is.null(dat1$kingdom)) indfu$Kingdom[i] <- dat1$kingdom
         if(!is.null(dat1$phylum)) indfu$Phylum[i] <- dat1$phylum
         if(!is.null(dat1$class)) indfu$Class[i] <- dat1$class
@@ -70,17 +71,19 @@ gettaxo <- function(indf,genus=FALSE){
         if(!is.null(dat1$genus)) indfu$Genus[i] <- dat1$genus
       }
       sciname <- Scientific_name
+      if(!verbose) {setTxtProgressBar(pb, i)}
     }
   }
+  if(!verbose) {setTxtProgressBar(pb, pbar)}
   indfu=rbind(indfu,indfu1)
   indf=sqldf("select * from indf, indfu where indf.Scientific_name = indfu.Scientific_name")
   if(file.exists("taxo.db")){
-    print("Updating local database with new names")
+    cat("\nUpdating local database with new names\n")
     sqldf("insert into taxo select * from indfu where Scientific_name not in (select Scientific_name from taxo)",dbname="taxo.db")
   } else {
     sqldf("attach 'taxo.db' as new")
-    sqldf("create table taxo as select * from indfu", ,dbname="taxo.db")
-    print("Creating local database taxo.db ...")
+    sqldf("create table taxo as select * from indfu", dbname="taxo.db")
+    cat("\nCreating local database taxo.db ...\n")
   } 
   indf <- indf[, !duplicated(colnames(indf))]
   names(indf)[names(indf)=="Order"]<-"Order_"
