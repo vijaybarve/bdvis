e59acad2dd7eb31d746c63402d176cd089876457
VijayBarve <vjaybarve@rediffmail.com>
2016-11-09 22:08:47 -0500

    V 0.2.9 fxn bdcomplete now returns number of records per grid cell #83

--- R/bdcomplete.R
+++ R/bdcomplete.R
@@ -23,42 +23,43 @@
 #'  calculations. Default is 50. If there are too few records, the function 
 #'  throws an error.
 #'@return data.frame with the columns \itemize{ \item{"Cell_id"}{id of the cell}
-#'  \item{"Sobs"}{Number of Observed species} \item{"Sest"}{Estimated number of 
-#'  species} \item{"c"}{Completeness ratio the cell} Plots a graph of Number of 
-#'  species vs completeness }
+#'  \item{"nrec"}{Number of records in the cell}\item{"Sobs"}{Number of Observed 
+#'  species} \item{"Sest"}{Estimated number of species} \item{"c"}{Completeness 
+#'  ratio the cell} Plots a graph of Number of species vs completeness }
 #'@examples \dontrun{
 #'bdcomplete(inat)
 #'}
 #'@seealso \code{\link{getcellid}}
 #'@export
 bdcomplete <- function(indf,recs=50){
-  dat1=sqldf("select Scientific_name, Date_collected, Cell_id from indf group by Scientific_name, Date_collected, Cell_id")
-  dat2=sqldf("select cell_id,count(*) as cell_ct from dat1 group by cell_id")
-  dat3=sqldf(paste("select * from dat2 where cell_ct > ",recs))
-  dat1=na.omit(dat1)
-  dat2=na.omit(dat2)
-  dat3=na.omit(dat3)
-  retmat=NULL
+  dat1 <- sqldf("select Scientific_name, Date_collected, Cell_id from indf group by Scientific_name, Date_collected, Cell_id")
+  dat2 <- sqldf("select cell_id,count(*) as cell_ct from dat1 group by cell_id")
+  dat3 <- sqldf(paste("select * from dat2 where cell_ct > ",recs))
+  dat1 <- na.omit(dat1)
+  dat2 <- na.omit(dat2)
+  dat3 <- na.omit(dat3)
+  retmat <- NULL
   if(dim(dat3)[1]<1){
     stop("Too few data records to compute completeness")
-    #return(NULL)
   }
   for (i in 1:dim(dat3)[1]){
-    Cell_id=dat3$Cell_id[i]
-    cset = dat1[which(dat1$Cell_id==dat3$Cell_id[i]),]
-    csum = sqldf("select Scientific_name, count(*) as sct from cset group by Scientific_name")
-    Q1=as.numeric(sqldf("select count(*) from csum where sct = 1 "))
-    Q2=sqldf("select count(*) from csum where sct = 2 ")
-    m=sqldf("select count(*) from ( select * from cset group by Date_collected )")
-    Sobs = as.numeric(sqldf("select count(*) from ( select * from cset group by Scientific_name)"))
+    Cell_id <- dat3$Cell_id[i]
+    nrec <- dat3$cell_ct[i]
+    cset <- dat1[which(dat1$Cell_id==dat3$Cell_id[i]),]
+    csum <- sqldf("select Scientific_name, count(*) as sct from cset group by Scientific_name")
+    Q1 <- as.numeric(sqldf("select count(*) from csum where sct = 1 "))
+    Q2 <- sqldf("select count(*) from csum where sct = 2 ")
+    m <- sqldf("select count(*) from ( select * from cset group by Date_collected )")
+    Sobs <- as.numeric(sqldf("select count(*) from ( select * from cset group by Scientific_name)"))
     if(Sobs>0){
-      Sest = as.numeric(Sobs + (((m-1)/m) * ((Q1 *(Q1 -1))/(2 * (Q2+1)))))
-      c = Sobs / Sest
-      retmat=rbind(retmat,c(Cell_id,Sobs,Sest,c))
+      Sest <- as.numeric(Sobs + (((m-1)/m) * ((Q1 *(Q1 -1))/(2 * (Q2+1)))))
+      c <- Sobs / Sest
+      retmat <- rbind(retmat,c(Cell_id,nrec,Sobs,Sest,c))
     }
   }
-  retmat=as.data.frame(retmat)
-  names(retmat)=c("Cell_id","Sobs","Sest","c")
-  plot(retmat$Sobs,retmat$c,main="Completeness vs number of species",xlab="Number of species",ylab="Completeness")
+  retmat <- as.data.frame(retmat)
+  names(retmat) <- c("Cell_id","nrec","Sobs","Sest","c")
+  plot(retmat$Sobs, retmat$c, main="Completeness vs number of species", 
+       xlab="Number of species", ylab="Completeness")
   return(retmat)
 }
