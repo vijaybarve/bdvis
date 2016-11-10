d11971ac4604616c73bb869b4d23fdafcfc2dea5
VijayBarve <vjaybarve@rediffmail.com>
2016-11-09 19:17:56 -0500

    V 0.2.8 fxn chronohorogram has parameter ptsize to control point size #84

--- R/chronohorogram.R
+++ R/chronohorogram.R
@@ -14,6 +14,7 @@
 #'@param colors Pair of colors to build color gradient, in the form of a
 #'  character vector. Default is blue (less) - red (more) gradient
 #'  \code{c("red", "blue")}
+#'@param ptsize point size atjustment factor. Dafault is 1
 #'@references Arino, A. H., & Otegui, J. (2008). Sampling biodiversity sampling.
 #'  In Proceedings of TDWG (pp. 77-78). Retrieved from 
 #'  http://www.tdwg.org/fileadmin/2008conference/documents/Proceedings2008.pdf#page=77
@@ -24,26 +25,36 @@
 #'@examples \dontrun{
 #'chronohorogram(inat)
 #'}
-chronohorogram <- function (indf=NA,title="Chronohorogram",startyear=1980,endyear=NA,
-                            colors=c("red", "blue")){
-  if(!is.element("Date_collected",names(indf))){
+chronohorogram <- function (indf = NA, title = "Chronohorogram", startyear = 1980, 
+                            endyear = NA, colors = c("red", "blue"), ptsize = 1) 
+{
+  if (!is.element("Date_collected", names(indf))) {
     stop("Field Date_collected not found in dataset \n")
   }
-  if (is.na(endyear)){
-    endyear=as.integer(format(Sys.Date(), "%Y"))
+  if (is.na(endyear)) {
+    endyear = as.integer(format(Sys.Date(), "%Y"))
   }
-  dat1=sqldf("select Date_collected, count(*) as ct from indf group by Date_collected")
-  if(is.na(dat1$Date_collected[1])){dat1=dat1[2:dim(dat1)[1],]}
-  if(as.character(dat1$Date_collected[1])==""){dat1=dat1[2:dim(dat1)[1],]}
-  d=as.numeric(strftime(as.Date(dat1$Date_collected,na.rm=T), format = "%j"))
-  y=as.numeric(strftime(as.Date(dat1$Date_collected,na.rm=T), format = "%Y"))
-  rind=which(y<=endyear & y>=startyear)
-  d=d[rind]
-  y=y[rind]
-  crp=colorRampPalette(colors)( round(log10(max(dat1$ct)+2)) +1 ) 
-  radial.plot(y,(d/366)*360,rp.type="s", start=1.62, labels=month.abb,
-              clockwise=TRUE, point.col=crp[(round(log10(dat1[,2])+2))-1], 
-              point.symbols=20, grid.bg="black",radial.lim=c(startyear,endyear),
-              show.radial.grid=T,show.grid.labels=F,show.grid=T,
-              main=title)
+  dat1 = sqldf("select Date_collected, count(*) as ct from indf group by Date_collected")
+  if (is.na(dat1$Date_collected[1])) {
+    dat1 = dat1[2:dim(dat1)[1], ]
+  }
+  if (as.character(dat1$Date_collected[1]) == "") {
+    dat1 = dat1[2:dim(dat1)[1], ]
+  }
+  d = as.numeric(strftime(as.Date(dat1$Date_collected, na.rm = T), 
+                          format = "%j"))
+  y = as.numeric(strftime(as.Date(dat1$Date_collected, na.rm = T), 
+                          format = "%Y"))
+  rind = which(y <= endyear & y >= startyear)
+  d = d[rind]
+  y = y[rind]
+  crp = colorRampPalette(colors)(round(log10(max(dat1$ct) + 
+                                               2)) + 1)
+  radial.plot(y, (d/366) * 360, rp.type = "s", start = 1.62, 
+              labels = month.abb, clockwise = TRUE, 
+              point.col = crp[(round(log10(dat1[,2]) + 2)) - 1], 
+              point.symbols = 20, grid.bg = "black", 
+              radial.lim = c(startyear, endyear),  cex=ptsize, 
+              show.radial.grid = T, show.grid.labels = F, show.grid = T, 
+              main = title)
 }
